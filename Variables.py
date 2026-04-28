b3c14c974cd3c94f7c7a2e70cc668fef8d1b68f8
Jean2012M <Jeanpaulmejia20@gmail.com>
2026-04-27 22:49:55 -0500

    Update and rename README.md to Variables.py

---
+++ Variables.py
@@ -0,0 +1,16 @@
+# Mi_primer_Repositorio-#Inicializando variables y colocandoles un valor
+nombre= "Jean Paul Mejia Angarita"
+edad= 14
+Estado= True
+
+#Esta linea me imprime el contenido de una variable
+print("Bienvenido", nombre)
+print("Tu edad es", edad)
+
+#Uso de la funcion input()
+nombre=input("¿Cual es tu nombre? ")
+edad=input("¿Cual es tu edad? ")
+
+#Mostrar el resultado capturado con input()
+print("Bienvenido", nombre)
+print("Tu edad es", edad)
